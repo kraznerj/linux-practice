e9e25214918b273e9821d87603c34a9582e83705
jori <jorikrasner@gmail.com>
2025-11-26 09:11:58 -0600

    fixing directory underscores to dashes

--- linux-practice.sh
+++ linux-practice.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # Create directories
-mkdir -p ~/linux_practice/{test1,test2,test3}
+mkdir -p ~/linux-practice/{test1,test2,test3}
 
 # Create test files
-touch ~/linux_practice/test1/file1.txt
-touch ~/linux_practice/test2/file2.txt
-touch ~/linux_practice/test3/file3.txt
+touch ~/linux-practice/test1/file1.txt
+touch ~/linux-practice/test2/file2.txt
+touch ~/linux-practice/test3/file3.txt
 
-echo -e "This is file 1." > ~/linux_practice/test1/file1.txt
-echo -e "Jori Was Here" > ~/linux_practice/test2/file2.txt
-echo -e "This is file 3." > ~/linux_practice/test3/file3.txt
+echo -e "This is file 1." >> ~/linux-practice/test1/file1.txt
+echo -e "Jori Was Here" >> ~/linux-practice/test2/file2.txt
+echo -e "This is file 3." >> ~/linux-practice/test3/file3.txt
 
 # Set permissions
-chmod 644 ~/linux_practice/test1/file1.txt
-chmod 007 ~/linux_practice/test2/file2.txt
-chmod 755 ~/linux_practice/test3/file3.txt
+chmod 644 ~/linux-practice/test1/file1.txt
+chmod 007 ~/linux-practice/test2/file2.txt
+chmod 755 ~/linux-practice/test3/file3.txt
 
 # Create script file
-echo -e '#\!/bin/bash\n\necho "Hello, World!"' > ~/linux_practice/hello.sh
+echo -e '#\!/bin/bash\n\necho "Hello, World!"' > ~/linux-practice/hello.sh
 
 
